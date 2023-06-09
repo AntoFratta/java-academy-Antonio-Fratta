266883a98794313fb98899699e42e1371ad8c9d2
AntoFratta <a.fratta3@studenti.uniba.it>
2023-06-09 16:00:09 +0200

    fix format

--- TestFinale_AFratta/src/esercizio1/MainTest.java
+++ TestFinale_AFratta/src/esercizio1/MainTest.java
@@ -3,17 +3,17 @@ package esercizio1;
 public class MainTest {
 
 	public static void main(String[] args) {
-		
-		int[] arr = {547, 87, 1, 24, 4, 9, 54, 37, 26, 19}; 
+
+		int[] arr = { 547, 87, 1, 24, 4, 9, 54, 37, 26, 19 };
 		int sum = 0;
 		double media = 0;
-		
-		for(int i=0; i<arr.length; i++) {
-			sum+= arr[i];
+
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
 		}
-		
-		media = (double)sum/arr.length;
-		System.out.println("La media è: "+media);
+
+		media = (double) sum / arr.length;
+		System.out.println("La media è: " + media);
 	}
 
 }
